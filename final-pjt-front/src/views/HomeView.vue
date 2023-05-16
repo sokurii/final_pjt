133b30d537642f1d4915e071e46790cb13ac5860
박진희 <wlsl0620@naver.com>
2023-05-17 07:56:20 +0900

    레이아웃수정

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -53,38 +53,53 @@
 
     </div>
 
-    <div class="index-contents">
       <div class="section-1">
         <div class="inner">
-          <div class="img"><img src="../assets/coin.png" alt=""></div>
-          <div class="copy-box">
-            <div class="title">예/적금금리비교</div>
-            <div class="copy">전국 금융기관의 예금, 적금 정보를<br>한 눈에 알려 드려요!</div>
-            <router-link to="/finance" class="btn btn-secondary">금융상품비교하기</router-link> 
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <img src="../assets/coin.png" alt="">
+          </div>
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <div class="copy-box">
+              <div class="title">예/적금금리비교</div>
+              <div class="copy">전국 금융기관의 예금, 적금 정보를<br>한 눈에 알려 드려요!</div>
+              <router-link to="/finance" class="btn btn-secondary">금융상품비교하기</router-link> 
+            </div>
           </div>
         </div>
       </div>
+
 
       <div class="section-2">
         <div class="inner">
-          <div class="copy-box">
-            <div class="title">환율계산기</div>
-            <div class="copy">지금 환율은 얼마? </div>
-            <router-link to="/finance" class="btn btn-secondary">환율계산 하러가기</router-link> 
-          <!-- <div class="img"><img src="../assets/coin.png" alt=""></div>             -->
-        </div>
-      </div>
-
-      <div class="section-3">
-        <div class="inner">
-          <div class="copy-box">
-            <div class="title">우리동네은행찾기</div>
-            <div class="copy">지금 가장 가까운 은행은 어디?</div>
-            <router-link to="/map" class="btn btn-secondary">근처 은행 찾아보기</router-link> 
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <div class="copy-box">
+              <div class="title">환율계산기</div>
+              <div class="copy">내게 꼭 필요했던 환율,<br>ㅇㅇ에게 맡겨 보세요!</div>
+              <router-link to="/finance" class="btn btn-secondary">환율계산하러가기</router-link> 
+            </div>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <img src="../assets/cal.png" alt="" class="img-fluid mx-auto d-block" style="max-width: 50%; max-height: 50%;">
           </div>
         </div>
       </div>
-    </div>
+
+
+
+      <div class="section-3">
+        <div class="inner">
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <img src="../assets/bank.png" alt="" class="img-fluid mx-auto d-block" style="max-width: 50%; max-height: 50%;">>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <div class="copy-box">
+              <div class="title">우리동네은행</div>
+              <div class="copy">마음에 드는 상품을 어디서 가입할까?<br>주변 은행을 찾아보세요!</div>
+              <router-link to="/finance" class="btn btn-secondary">우리동네은행찾기</router-link> 
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
@@ -118,12 +133,17 @@ export default {
 }
 
 .section-2{
-  
+  display:flex;
+  justify-content: center;
+  align-items: center;  
   height: 500px;
   background-color: white;
 }
 
 .section-3{
+  display:flex;
+  justify-content: center;
+  align-items: center;
   height: 500px;
   background-color: #F3F2E9;;
   /* background-color: #ebb262; */
@@ -132,6 +152,10 @@ export default {
 .inner{
   width: 1060px;
   height: 500px;
+  display: flex;
+  /* flex-direction: column;
+  justify-content: center;
+  align-items: center; */
   /* background-color: #fff; */
 
 }
@@ -142,20 +166,27 @@ export default {
 }
 
 .copy-box{
-  float: right;
-  padding-right: 60px;
-  
-
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
 
 }
-.title{
+
+
+.copy-box .title{
   font-size : 36px;
   font-weight: 800;
-
+  text-align: left;
 }
 
-.copy{
+.copy-box .copy{
   text-align: left;
   font-weight: 500;
+  text-align: left;
+}
+
+.copy-box .btn{
+  align-self:flex-start;
 }
 </style>
