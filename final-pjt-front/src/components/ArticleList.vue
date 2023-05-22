a3d8450c644bea99f533987f6996a9eca2b6a185
박진희 <wlsl0620@naver.com>
2023-05-22 17:53:36 +0900

    articlelist(시작)

--- final-pjt-front/src/components/ArticleList.vue
+++ final-pjt-front/src/components/ArticleList.vue
@@ -1,30 +1,61 @@
 <template>
-  <div>
-    <h1>게시글 목록</h1>
-    <!-- <h3 v-if="noArticles">생성된 게시글이 없습니다.</h3> -->
+  <div class="bg-white">
+    <h1 class="p-3">게시글 목록</h1>
     <router-link :to="{ name: 'CreateArticle' }" class="btn btn-primary">게시글 작성하기</router-link>
-    <ArticleListItem 
-      v-for="article in articles"
-      :key="article.id"
-      :article="article"
-    />
+    
+    <div class="board_container">
+      <b-table striped hover :items="articles" :fields="fields" :per-page="perPage">
+        <template #cell(title)="{ item }">
+          <router-link :to="{ name: 'DetailArticle', params: { id: item.id } }">{{ item.title }}</router-link>
+        </template>
+        <template #cell(username)="{ item }">
+          {{ item.username }}
+        </template>
+        <template #cell(created_at)="{ item }">
+          {{ item.created_at }}
+        </template>
+      </b-table>
+
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="articles.length"
+        :per-page="perPage"
+        align="center"
+        class="mt-3"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
 <script>
+import { BTable, BPagination } from 'bootstrap-vue'
 import ArticleListItem from './ArticleListItem.vue'
+
 export default {
   name: 'ArticleList',
   components: {
+    BTable,
+    BPagination,
     ArticleListItem,
+  },
+  data() {
+    return {
+      currentPage: 1,
+      perPage: 10,
+      fields: [
+        { key: 'title', label: '제목' },
+        { key: 'username', label: '작성자' },
+        { key: 'created_at', label: '날짜' },
+      ],
+    }
   },
   computed: {
     articles() {
       return this.$store.state.articles
     },
-    noArticles() {
-      return this.$store.state.articles && this.$store.state.articles.length === 0
-    },
+    // noArticles() {
+    //   return this.$store.state.articles && this.$store.state.articles.length === 0
+    // },
   },
   created() {
     this.getArticles()
@@ -32,11 +63,14 @@ export default {
   methods: {
     getArticles() {
       this.$store.dispatch('getArticles')
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style>
-
+.board_container{
+  margin: 50px 100px 200px;
+  background-color: #fff;
+}
 </style>
